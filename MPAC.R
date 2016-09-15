f27dc09ea7a1e86a75b256b8a10cf7ece5e63212
Stephanie Wong <stephanie.wong@mpac.ca>
2016-09-15 09:49:13 -0400

    Add in reading a file called validate-vars.txt with column classes

--- MPAC.R
+++ MPAC.R
@@ -1,3 +1,10 @@
+# Read in the  (fake) 2020.csv file
+# Read in a text file and make sure that the first column is character
+# http://stackoverflow.com/questions/2805357/specifying-colclasses-in-the-read-csv
+column_classes=c("character","numeric","character")
+data<-read.csv("validate-vars.txt", colClasses=column_classes)
+
+
 #frequency counts
 #compare means
 #cross tabs
